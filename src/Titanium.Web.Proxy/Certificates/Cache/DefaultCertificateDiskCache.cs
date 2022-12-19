d8e32180caf0ca75b27eaac060f8015eaff46b78
Piero Viano <piero.viano@gmail.com>
2022-12-19 04:34:01 +0200

    20221219

--- src/Titanium.Web.Proxy/Certificates/Cache/DefaultCertificateDiskCache.cs
+++ src/Titanium.Web.Proxy/Certificates/Cache/DefaultCertificateDiskCache.cs
@@ -4,115 +4,117 @@ using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using Titanium.Web.Proxy.Helpers;
 
-namespace Titanium.Web.Proxy.Network;
-
-public sealed class DefaultCertificateDiskCache : ICertificateCache
+namespace Titanium.Web.Proxy.Network
 {
-    private const string DefaultCertificateDirectoryName = "crts";
-    private const string DefaultCertificateFileExtension = ".pfx";
-    private const string DefaultRootCertificateFileName = "rootCert" + DefaultCertificateFileExtension;
-    private string? rootCertificatePath;
 
-    public X509Certificate2? LoadRootCertificate(string pathOrName, string password, X509KeyStorageFlags storageFlags)
+    public sealed class DefaultCertificateDiskCache : ICertificateCache
     {
-        var path = GetRootCertificatePath(pathOrName);
-        return LoadCertificate(path, password, storageFlags);
-    }
+        private const string DefaultCertificateDirectoryName = "crts";
+        private const string DefaultCertificateFileExtension = ".pfx";
+        private const string DefaultRootCertificateFileName = "rootCert" + DefaultCertificateFileExtension;
+        private string? rootCertificatePath;
 
-    public void SaveRootCertificate(string pathOrName, string password, X509Certificate2 certificate)
-    {
-        var path = GetRootCertificatePath(pathOrName);
-        var exported = certificate.Export(X509ContentType.Pkcs12, password);
-        File.WriteAllBytes(path, exported);
-    }
-
-    /// <inheritdoc />
-    public X509Certificate2? LoadCertificate(string subjectName, X509KeyStorageFlags storageFlags)
-    {
-        var filePath = Path.Combine(GetCertificatePath(false), subjectName + DefaultCertificateFileExtension);
-        return LoadCertificate(filePath, string.Empty, storageFlags);
-    }
-
-    /// <inheritdoc />
-    public void SaveCertificate(string subjectName, X509Certificate2 certificate)
-    {
-        var filePath = Path.Combine(GetCertificatePath(true), subjectName + DefaultCertificateFileExtension);
-        var exported = certificate.Export(X509ContentType.Pkcs12);
-        File.WriteAllBytes(filePath, exported);
-    }
-
-    public void Clear()
-    {
-        try
+        public X509Certificate2? LoadRootCertificate(string pathOrName, string password,
+            X509KeyStorageFlags storageFlags)
         {
-            var path = GetCertificatePath(false);
-            if (Directory.Exists(path)) Directory.Delete(path, true);
-        }
-        catch (Exception)
-        {
-            // do nothing
-        }
-    }
-
-    private X509Certificate2? LoadCertificate(string path, string password, X509KeyStorageFlags storageFlags)
-    {
-        byte[] exported;
-
-        if (!File.Exists(path)) return null;
-
-        try
-        {
-            exported = File.ReadAllBytes(path);
-        }
-        catch (IOException)
-        {
-            // file or directory not found
-            return null;
+            var path = GetRootCertificatePath(pathOrName);
+            return LoadCertificate(path, password, storageFlags);
         }
 
-        return new X509Certificate2(exported, password, storageFlags);
-    }
-
-    private string GetRootCertificatePath(string pathOrName)
-    {
-        if (Path.IsPathRooted(pathOrName)) return pathOrName;
-
-        return Path.Combine(GetRootCertificateDirectory(),
-            string.IsNullOrEmpty(pathOrName) ? DefaultRootCertificateFileName : pathOrName);
-    }
-
-    private string GetCertificatePath(bool create)
-    {
-        var path = GetRootCertificateDirectory();
-
-        var certPath = Path.Combine(path, DefaultCertificateDirectoryName);
-        if (create && !Directory.Exists(certPath)) Directory.CreateDirectory(certPath);
-
-        return certPath;
-    }
-
-    private string GetRootCertificateDirectory()
-    {
-        if (rootCertificatePath == null)
+        public void SaveRootCertificate(string pathOrName, string password, X509Certificate2 certificate)
         {
-            if (RunTime.IsUwpOnWindows)
+            var path = GetRootCertificatePath(pathOrName);
+            var exported = certificate.Export(X509ContentType.Pkcs12, password);
+            File.WriteAllBytes(path, exported);
+        }
+
+        /// <inheritdoc />
+        public X509Certificate2? LoadCertificate(string subjectName, X509KeyStorageFlags storageFlags)
+        {
+            var filePath = Path.Combine(GetCertificatePath(false), subjectName + DefaultCertificateFileExtension);
+            return LoadCertificate(filePath, string.Empty, storageFlags);
+        }
+
+        /// <inheritdoc />
+        public void SaveCertificate(string subjectName, X509Certificate2 certificate)
+        {
+            var filePath = Path.Combine(GetCertificatePath(true), subjectName + DefaultCertificateFileExtension);
+            var exported = certificate.Export(X509ContentType.Pkcs12);
+            File.WriteAllBytes(filePath, exported);
+        }
+
+        public void Clear()
+        {
+            try
             {
-                rootCertificatePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                var path = GetCertificatePath(false);
+                if (Directory.Exists(path)) Directory.Delete(path, true);
             }
-            else if (RunTime.IsLinux)
+            catch (Exception)
             {
-                rootCertificatePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                // do nothing
             }
-            else if (RunTime.IsMac)
-            {
-                rootCertificatePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            }
-            else
-            {
-                var assemblyLocation = GetType().Assembly.Location;
+        }
 
-                // dynamically loaded assemblies returns string.Empty location
-                if (assemblyLocation == string.Empty) assemblyLocation = Assembly.GetEntryAssembly().Location;
+        private X509Certificate2? LoadCertificate(string path, string password, X509KeyStorageFlags storageFlags)
+        {
+            byte[] exported;
+
+            if (!File.Exists(path)) return null;
+
+            try
+            {
+                exported = File.ReadAllBytes(path);
+            }
+            catch (IOException)
+            {
+                // file or directory not found
+                return null;
+            }
+
+            return new X509Certificate2(exported, password, storageFlags);
+        }
+
+        private string GetRootCertificatePath(string pathOrName)
+        {
+            if (Path.IsPathRooted(pathOrName)) return pathOrName;
+
+            return Path.Combine(GetRootCertificateDirectory(),
+                string.IsNullOrEmpty(pathOrName) ? DefaultRootCertificateFileName : pathOrName);
+        }
+
+        private string GetCertificatePath(bool create)
+        {
+            var path = GetRootCertificateDirectory();
+
+            var certPath = Path.Combine(path, DefaultCertificateDirectoryName);
+            if (create && !Directory.Exists(certPath)) Directory.CreateDirectory(certPath);
+
+            return certPath;
+        }
+
+        private string GetRootCertificateDirectory()
+        {
+            if (rootCertificatePath == null)
+            {
+                if (RunTime.IsUwpOnWindows)
+                {
+                    rootCertificatePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                }
+                else if (RunTime.IsLinux)
+                {
+                    rootCertificatePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                }
+                else if (RunTime.IsMac)
+                {
+                    rootCertificatePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                }
+                else
+                {
+                    var assemblyLocation = GetType().Assembly.Location;
+
+                    // dynamically loaded assemblies returns string.Empty location
+                    if (assemblyLocation == string.Empty) assemblyLocation = Assembly.GetEntryAssembly().Location;
 
 #if NETSTANDARD2_1
                     // single-file app returns string.Empty location
@@ -122,12 +124,13 @@ public sealed class DefaultCertificateDiskCache : ICertificateCache
                     }
 #endif
 
-                var path = Path.GetDirectoryName(assemblyLocation);
+                    var path = Path.GetDirectoryName(assemblyLocation);
 
-                rootCertificatePath = path ?? throw new NullReferenceException();
+                    rootCertificatePath = path ?? throw new NullReferenceException();
+                }
             }
-        }
 
-        return rootCertificatePath;
+            return rootCertificatePath;
+        }
     }
 }
