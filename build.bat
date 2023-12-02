f7bae833aa0862bf62f2f7f9fc1219a76d01e688
Piero Viano <piero.viano@gmail.com>
2023-12-02 04:42:35 +0200

    ''

--- build.bat
+++ build.bat
@@ -1,4 +1,11 @@
-@echo off
-
-powershell -NoProfile -ExecutionPolicy bypass -Command "%~dp0.build\setup.ps1 %*; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
-exit /B %errorlevel%
+cd src
+del ..\..\WhenTheVersion\Packages\Titanium.Web.Proxy.PieroViano.*
+rmdir /s /q %userprofile%\.nuget\Packages\Titanium.Web.Proxy.PieroViano
+MSBuild.exe Titanium.Web.Proxy.sln -t:restore -p:RestorePackagesConfig=true
+MSBuild.exe Titanium.Web.Proxy.sln -m /property:Configuration=%Configuration% 
+copy "Titanium.Web.Proxy\bin\%Configuration%\Titanium.Web.Proxy.PieroViano.*.nupkg" ..\..\WhenTheVersion\Packages\
+git push
+git add -A
+git commit -a --allow-empty-message -m ''
+git push
+cd ..\
